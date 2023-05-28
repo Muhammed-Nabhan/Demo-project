5873e521def13b95892ba0129d91e4cb95ade14a
Muhammed Nabhan <muhammednabhan2301@gmail.com>
2023-05-28 06:15:18 +0530

    FIXING quotes API

--- script.js
+++ script.js
@@ -70,8 +70,9 @@ function getDominantEmotion(emotions) {
 }
 
 // Fetch a random quote based on the detected emotion
+// Fetch a random quote based on the detected emotion
 function fetchQuoteByEmotion(emotion) {
-  const apiUrl = "https://zenquotes.io/api/random";
+  const apiUrl = `https://zenquotes.io/api/quotes?emotion=${emotion}&orderBy=random`;
 
   fetch(apiUrl)
     .then(response => response.json())
